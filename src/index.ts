f572cf7b2295132bb5298ae34b54fffdbab5b9b8
Fabrício dos Reis Ferreira <fabricioreisferreira@hotmail.com>
2026-04-26 15:00:50 -0300

    create startBot function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
 import makeWASocket, {
   fetchLatestBaileysVersion,
+  useMultiFileAuthState,
   DisconnectReason,
-} from "@whiskeysockets/baileys";
+} from "baileys";
+import QRCode from "qrcode";
+
+const startBot = async () => {
+  const { version } = await fetchLatestBaileysVersion();
+  const { state, saveCreds } = await useMultiFileAuthState("auth");
+
+  const sock = makeWASocket({
+    auth: state,
+    version,
+  });
+
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect, qr } = update;
+    if (qr) {
+      console.log(await QRCode.toString(qr, { type: "terminal" }));
+    }
+    if (
+      connection === "close" &&
+      lastDisconnect?.error?.output?.statusCode ===
+        DisconnectReason.restartRequired
+    ) {
+      startBot();
+    }
+  });
+
+  sock.ev.on("messages.upsert", ({ type, messages }) => {
+    if (type === "notify") {
+      // New messages
+      for (const message of messages) {
+        console.log("msg:", message);
+      }
+    } else {
+      // Old messages
+    }
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+};
+
+startBot();
